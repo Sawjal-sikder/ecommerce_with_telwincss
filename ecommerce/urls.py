bdac9bed0e2fac95285a4eed45e592c23a769b7f
Md Sawjal Sikder <134833280+Sawjal-sikder@users.noreply.github.com>
2024-10-13 06:52:44 +0600

    form category

--- ecommerce/urls.py
+++ ecommerce/urls.py
@@ -5,7 +5,8 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('home.urls'))
+    path('', include('home.urls')),
+    path('product/', include('product.urls'))
 ]
 
 if settings.DEBUG:
--- product/views.py
+++ product/views.py
@@ -1,3 +1,16 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib import messages
+from .forms import CategoryForm
 
-# Create your views here.
+
+def categoryViews(request):
+    if request.method == "POST":
+        form = CategoryForm(request.POST)
+        if form.is_valid():
+            form.save()
+            messages.success(request, "Category added successfully!")
+            return redirect('category')
+    else:
+        form = CategoryForm()
+
+    return render(request, 'category.html', {'form': form})
